aea270aa73b252c6ec9dc15fae546acb6d77af53
Elanna Grossman <elanna.grossman@gmail.com>
2017-05-05 21:27:43 -0700

    feat(routes): require passport

--- app/routes/users.js
+++ app/routes/users.js
@@ -4,6 +4,7 @@ let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 let User = mongoose.model('User');
+let passport = require('passport');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
