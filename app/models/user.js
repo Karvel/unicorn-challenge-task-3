fc1fe8a3ff712dc3eab2942ef1f95edee5a5d0bd
Elanna Grossman <elanna.grossman@gmail.com>
2017-05-05 21:14:15 -0700

    feat(user): salt password

--- app/models/user.js
+++ app/models/user.js
@@ -6,4 +6,28 @@ const UserSchema = new mongoose.Schema({
 	password : String,
 });
 
+UserSchema.pre('save', function (next) {
+	if (!this.isModified('password')) {
+		next();
+	} else {
+		//generate a salt
+		bcrypt.genSaltAsync(10)
+			.then(salt => {
+				//generate a hashed password
+				bcrypt.hashAsync(this.password, salt)
+					.then(hash => {
+						this.password = hash;
+						next();
+					})
+					.catch(next);
+			})
+			.catch(next);
+	}
+});
+
+UserSchema.methods.toJSON = function () {
+	let user = _.omit(this.toObject(), ['password', '__v']);
+	return user;
+};
+
 mongoose.model('User', UserSchema);
