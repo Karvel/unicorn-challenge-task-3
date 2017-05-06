f4ae211e0faf69b00171227684fca10be0bd48b7
Elanna Grossman <elanna.grossman@gmail.com>
2017-05-05 19:20:50 -0700

    feat(app): add passport requires

--- app/app.js
+++ app/app.js
@@ -14,6 +14,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
