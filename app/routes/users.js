664faab74002d5697767d1a4e0283e6779598bda
Elanna Grossman <elanna.grossman@gmail.com>
2017-05-05 19:10:02 -0700

    refactor(routes): use es6

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+let express = require('express');
+let router = express.Router();
 let mongoose = require('mongoose');
 let User = mongoose.model('User');
 
